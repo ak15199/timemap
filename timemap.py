0c6e68a7c993c2f6c81e94f8675ee5f4f7d4c611
Alex King <alex.king@captechconsulting.com>
2020-01-28 08:19:07 -0500

    First draft adding CA

--- timemap.py
+++ timemap.py
@@ -1,3 +1,4 @@
+from bokeh.colors.named import darkgray, firebrick, gray, lavender, white
 from bokeh.io import show, output_file
 from bokeh.models.ranges import DataRange1d
 from bokeh.palettes import viridis
@@ -9,20 +10,52 @@ import colorsys
 import click
 import csv
 
+from functools import reduce
+
 from matplotlib.colors import to_rgb, to_hex, hsv_to_rgb, rgb_to_hsv
 
 
-HGHT_LT = 0.8
-HGHT_PT = 0.6
-COLOR_BG = "#EAEAF2"
-
-def dim(hex, saturation=0.5):
+def dim(hex, saturation=1, value=1):
   rgb = to_rgb(hex)
   hsv = rgb_to_hsv(rgb)
-  rgb = hsv_to_rgb((hsv[0], hsv[1], hsv[2]*saturation))
+  rgb = hsv_to_rgb((hsv[0], min(1, hsv[1]*saturation), min(1, hsv[2]*value)))
 
   return to_hex(rgb)
 
+
+def bolden(color):
+  return dim(color, value=1.2, saturation=1.2)
+
+
+def darken(color):
+  return dim(color, value=0.9, saturation=0.9)
+
+
+def lighten(color):
+  return dim(color, value=1.3, saturation=0.4)
+
+
+def bar(p, y, offset, key, value, color, ca_threshold):
+  HGHT_LT = 0.8
+  HGHT_PT = 0.6
+
+  lt, pt, ca = value
+
+  if key == "Overall":
+    text = [f"{100/lt*pt:.2f}% Efficiency"]
+    p.rect(x=lt/2, y=y, width=lt, height=HGHT_LT, fill_alpha=0.4, color=color)
+    p.rect(x=lt-pt/2, y=y, width=pt, height=HGHT_PT, color=color)
+    p.text(x=lt-pt/2, y=y, text=text, text_color=white.to_hex(), text_baseline="middle")
+  elif lt:
+    color_ca = firebrick.to_hex() if ca <= ca_threshold else darkgray.to_hex()
+    if ca>0:
+      p.rect(x=offset+lt+30, y=y, width=60, height=0.8, color=color_ca, line_color=darken(color_ca))
+      j = p.text(x=offset+lt+5, y=y, text=[str(ca)], text_color="#ffffff", text_baseline="middle")
+    p.rect(x=offset+lt/2, y=y, width=lt, height=HGHT_LT, line_color=darken(color), color=lighten(color))
+    p.rect(x=offset+lt - pt/2, y=y, width=pt, height=HGHT_PT, line_color=darken(color), color=bolden(color))
+
+  return lt
+  
 
 def generate(data, title="Figure", width=1000, height=350, x_range=None):
   
@@ -30,15 +63,21 @@ def generate(data, title="Figure", width=1000, height=350, x_range=None):
   
   data["Overall"] = (
     sum(item[0] for item in data.values()),
-    sum(item[1] for item in data.values())
+    sum(item[1] for item in data.values()),
+    reduce(
+      (lambda x, y: x/y),
+       map(
+       (lambda x: x if x else 1),
+        map((lambda x: x[2]/100), data.values())))
   )
   
+  ca_threshold = sorted(d[2] for d in data.values() if d[2]>0)[:3][-1]
   rows = len(data)
   pal = viridis(rows)
   
   p = figure(title=title, plot_height=height, plot_width=width, y_range=list(reversed(data)))
   p.x_range.range_padding = 0
-  p.background_fill_color = COLOR_BG
+  p.background_fill_color = lavender
   p.grid.grid_line_alpha=1.0
   p.grid.grid_line_color = "white"
   p.xaxis.axis_label = 'hours'
@@ -46,27 +85,14 @@ def generate(data, title="Figure", width=1000, height=350, x_range=None):
 
   # If a maximum value is specified, then push out how far autoscaling will go
   if x_range is not None:
-    p.rect(x=x_range, y=0, width=1, height=1, fill_alpha=0.0, color=COLOR_BG)
+    p.rect(x=x_range, y=0, width=1, height=1, fill_alpha=0.0, color=lavender)
   
   index = rows-1
   offset = 0
 
-  for k, v in data.items():
-    lt, pt = v
-    y = index+0.5
-    color = pal[index]
-
-    if k == "Overall":
-      text = [f"{100/lt*pt:.2f}% Efficiency"]
-      p.rect(x=lt/2, y=y, width=lt, height=HGHT_LT, fill_alpha=0.4, color=color)
-      p.rect(x=lt-pt/2, y=y, width=pt, height=HGHT_PT, color=color)
-      p.text(x=lt-pt/2, y=y, text=text, text_color="#ffffff", text_baseline="middle")
-    else:
-      p.rect(x=offset+lt/2, y=y, width=lt, height=HGHT_LT, fill_alpha=0.4, color=color)
-      p.rect(x=offset+lt - pt/2, y=y, width=pt, height=HGHT_PT, line_color=dim(color, 0.8), color=color)
-  
+  for key, value in data.items():
+    offset = offset + bar(p, index+0.5, offset, key, value, pal[index], ca_threshold)
     index -= 1
-    offset = offset + lt
   
   show(p)
 
@@ -87,8 +113,13 @@ def main(file, title, width, height, xrange):
   with open(file, newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
-      print(row)
-      data[row[0]] = (int(row[1]), int(row[2]))
+      if row[0][0] == "#" or len(row) != 4:
+        pass
+
+      try:
+        data[row[0]] = (int(row[1]), int(row[2]), int(row[3]))
+      except IndexError:
+        print("Not enough columns: "+",".join(row))
 
   generate(data, title, width, height, xrange)
 
