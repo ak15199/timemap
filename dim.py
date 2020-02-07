6ca2940ecea78d33125bb50d94a0ed6072fd8fd1
Alex King <alex.king@captechconsulting.com>
2020-02-07 12:11:47 -0600

    PEP cleanup

--- dim.py
+++ dim.py
@@ -1,22 +1,22 @@
-import colorsys
 from matplotlib.colors import to_rgb, to_hex, hsv_to_rgb, rgb_to_hsv
 
 
 def dim(hex, saturation=1, value=1):
-  rgb = to_rgb(hex)
-  hsv = rgb_to_hsv(rgb)
-  rgb = hsv_to_rgb((hsv[0], min(1, hsv[1]*saturation), min(1, hsv[2]*value)))
+    rgb = to_rgb(hex)
+    hsv = rgb_to_hsv(rgb)
+    rgb = hsv_to_rgb(
+        (hsv[0], min(1, hsv[1] * saturation), min(1, hsv[2] * value)))
 
-  return to_hex(rgb)
+    return to_hex(rgb)
 
 
 def bolden(color):
-  return dim(color, value=1.2, saturation=1.2)
+    return dim(color, value=1.2, saturation=1.2)
 
 
 def darken(color):
-  return dim(color, value=0.9, saturation=0.9)
+    return dim(color, value=0.9, saturation=0.9)
 
 
 def lighten(color):
-  return dim(color, value=1.4, saturation=0.4)
+    return dim(color, value=1.4, saturation=0.4)
--- timemap.py
+++ timemap.py
@@ -2,16 +2,15 @@ from bokeh.colors.named import darkgray, gray, lavender, white
 from bokeh.colors.named import firebrick as COLOR_CA
 from bokeh.colors.named import goldenrod as COLOR_AR
 
-from bokeh.core.enums import LegendLocation
 from bokeh.io import show, output_file
 from bokeh.models import Label, Legend
 from bokeh.models.glyphs import Rect
 from bokeh.palettes import linear_palette
 from bokeh.plotting import figure
 
-from colorcet import rainbow as palette
-from collections import OrderedDict
 import click
+from collections import OrderedDict
+from colorcet import rainbow as palette
 import csv
 
 from functools import reduce
@@ -20,207 +19,219 @@ from dim import bolden, darken, lighten
 
 
 UNITS = {
-  "h": {"text": "hours", "factor": 1},
-  "d": {"text": "days", "factor": 8},
-  "w": {"text": "weeks", "factor": 40},
+    "h": {"text": "hours", "factor": 1},
+    "d": {"text": "days", "factor": 8},
+    "w": {"text": "weeks", "factor": 40},
 }
 OVERALL_LABEL = "Overall"
 
 
 def label(p, x, y, align, color, text):
-  LABEL_SIZE = "9pt"
-  LABEL_OFFSET = 2
+    LABEL_SIZE = "9pt"
+    LABEL_OFFSET = 2
 
-  x_offset = LABEL_OFFSET if align=="left" else -LABEL_OFFSET
-  
-  l = Label(
-    x=x, y=y,
-    x_offset=x_offset,
-    text=f" {text} ",
-    text_font_size=LABEL_SIZE, text_color=white,
-    text_baseline='middle', text_align=align,
-    background_fill_color=color, border_line_color=darken(color),
+    x_offset = LABEL_OFFSET if align == "left" else -LABEL_OFFSET
+
+    l = Label(
+        x=x, y=y,
+        x_offset=x_offset,
+        text=f" {text} ",
+        text_font_size=LABEL_SIZE, text_color=white,
+        text_baseline='middle', text_align=align,
+        background_fill_color=color, border_line_color=darken(color),
     )
-  
-  p.add_layout(l)
 
-  return l
+    p.add_layout(l)
+
+    return l
 
 
-def bar(p, y, offset, key, value, color, units, ca_threshold, ar_threshold, mask):
-  HGHT_LT = 0.8
-  HGHT_PT = 0.6
+def bar(p, y, offset, value, color, units, ca_threshold, ar_threshold, mask):
+    HGHT_LT = 0.8
+    HGHT_PT = 0.6
 
-  lt, pt, ca, ar = value
+    lt, pt, ca, ar = value
 
-  color_ca = COLOR_CA.to_hex() if not mask and ca <= ca_threshold else darkgray.to_hex()
-  if ca>0:
-    label(p, offset+lt, y, "left", color_ca, f"{ca:.0f}%")
-    
-  color_ar = COLOR_AR.to_hex() if not mask and ar >= ar_threshold else darkgray.to_hex()
-  if ar>0:
-    label(p, offset, y, "right", color_ar, f"{ar:.0f}{units}")
+    color_ca = COLOR_CA.to_hex() if not mask and ca <= ca_threshold else darkgray.to_hex()
+    if ca > 0:
+        label(p, offset + lt, y, "left", color_ca, f"{ca:.0f}%")
 
-  p.rect(x=offset+lt/2, y=y, width=lt, height=HGHT_LT,
-    line_color=darken(color), color=lighten(color))
-  p.rect(x=offset+lt - pt/2, y=y, width=pt, height=HGHT_PT,
-    line_color=darken(color), color=bolden(color))
+    color_ar = COLOR_AR.to_hex() if not mask and ar >= ar_threshold else darkgray.to_hex()
+    if ar > 0:
+        label(p, offset, y, "right", color_ar, f"{ar:.0f}{units}")
 
-  return lt
-  
+    p.rect(x=offset + lt / 2, y=y, width=lt, height=HGHT_LT,
+           line_color=darken(color), color=lighten(color))
+    p.rect(x=offset + lt - pt / 2, y=y, width=pt, height=HGHT_PT,
+           line_color=darken(color), color=bolden(color))
+
+    return lt
+
 
 def legend(p):
-  
-  def glyph(color):
-    return p.add_glyph(Rect(
-        x=0, y=0,
-        fill_color=color.to_hex(),
-        line_color=darken(color.to_hex())),
-      visible=False)
-    
-  l = Legend(
-    items=[
-      ("Long Lead-time", [glyph(COLOR_AR)]),
-      ("High Error Rate", [glyph(COLOR_CA)]),
-      ],
-    location="bottom_right", orientation="horizontal",
-    border_line_color=gray,
-    title='Priority Items to Address'
-  )
 
-  p.add_layout(l, "below")
+    def glyph(color):
+        return p.add_glyph(Rect(
+            x=0, y=0,
+            fill_color=color.to_hex(),
+            line_color=darken(color.to_hex())),
+                           visible=False)
+
+    l = Legend(
+        items=[
+            ("Long Lead-time", [glyph(COLOR_AR)]),
+            ("High Error Rate", [glyph(COLOR_CA)]),
+        ],
+        location="bottom_right", orientation="horizontal",
+        border_line_color=gray,
+        title='Priority Items to Address'
+    )
+
+    p.add_layout(l, "below")
 
 
 def plotarea(title, width, height, y_range, units):
-  PADDING_PERCENT = 0.1  # left-right margin to accomodate labels
+    PADDING_PERCENT = 0.1  # left-right margin to accomodate labels
 
-  output_file("output.html")
-  
-  p = figure(title=title, plot_height=height, plot_width=width, y_range=y_range)
-  p.x_range.range_padding = PADDING_PERCENT
-  p.x_range.range_padding_units = "percent"
-  p.y_range.range_padding = 0.1
-  p.background_fill_color = lavender
-  p.border_fill_color= lighten(lavender.to_hex())
-  p.grid.grid_line_alpha=1.0
-  p.grid.grid_line_color = white
-  p.xaxis.axis_label = UNITS[units]["text"]
-  p.yaxis.axis_label = None
+    output_file("output.html")
 
-  return p
+    p = figure(
+        title=title,
+        plot_height=height,
+        plot_width=width,
+        y_range=y_range)
+    p.x_range.range_padding = PADDING_PERCENT
+    p.x_range.range_padding_units = "percent"
+    p.y_range.range_padding = 0.1
+    p.background_fill_color = lavender
+    p.border_fill_color = lighten(lavender.to_hex())
+    p.grid.grid_line_alpha = 1.0
+    p.grid.grid_line_color = white
+    p.xaxis.axis_label = UNITS[units]["text"]
+    p.yaxis.axis_label = None
+
+    return p
 
 
 def generate(data, title, width, height, x_range, units, items):
-  """
-  Generate the chart
-  """
-  
-  p = plotarea(title, width, height, list(reversed(data)), units)
+    """
+    Generate the chart
+    """
 
-  # Add one to items that we retrieve in determining thresholds, since the final
-  # slot is going to get swallowed by the summary line
-  ca_threshold = sorted(d[2] for d in data.values() if d[2]>0)[:(items+1)][-1]
-  ar_threshold = sorted(d[3] for d in data.values() if d[3]>0)[-(items+1):][0]
+    p = plotarea(title, width, height, list(reversed(data)), units)
 
-  rows = len(data)
-  pal = linear_palette(palette, rows)
-  
-  # If a maximum value is specified, then push out how far the x axis will go
-  # this helps with apples-to-apples comparison between before and after charts
-  if x_range is not None:
-    p.rect(x=x_range, y=0, width=1, height=1, fill_alpha=0.0)
-  
-  index = rows-1
-  offset = 0
+    # Add one to items that we retrieve in determining thresholds, since the final
+    # slot is going to get swallowed by the summary line
+    ca_threshold = sorted(d[2] for d in data.values()
+                          if d[2] > 0)[:(items + 1)][-1]
+    ar_threshold = sorted(d[3] for d in data.values()
+                          if d[3] > 0)[-(items + 1):][0]
 
-  for key, value in data.items():
-    mask = (key == OVERALL_LABEL)
-    if mask:
-      offset = 0
+    rows = len(data)
+    pal = linear_palette(palette, rows)
 
-    offset = offset + bar(p, index+0.5, offset, key, value,
-                          pal[index], units, ca_threshold, ar_threshold, mask)
-    index -= 1
-  
-  legend(p)
+    # If a maximum value is specified, then push out how far the x axis will go
+    # this helps with apples-to-apples comparison between before and after
+    # charts
+    if x_range is not None:
+        p.rect(x=x_range, y=0, width=1, height=1, fill_alpha=0.0)
 
-  show(p)
+    index = rows - 1
+    offset = 0
+
+    for key, value in data.items():
+        mask = (key == OVERALL_LABEL)
+        if mask:
+            offset = 0
+
+        offset = offset + bar(p, index + 0.5, offset, value,
+                              pal[index], units, ca_threshold, ar_threshold, mask)
+        index -= 1
+
+    legend(p)
+
+    show(p)
 
 
 def convert(data, units):
-  factor = UNITS[units]["factor"]
+    factor = UNITS[units]["factor"]
 
-  return OrderedDict((key, (value[0]/factor, value[1]/factor, value[2])) for key, value in data.items())
-  
+    return OrderedDict(
+        (key, (value[0] / factor, value[1] / factor, value[2])) for key, value in data.items())
+
 
 def augment(data):
-  """
-  Add summary/roll-up data to the chart, and calculate activity ratio
-  """
-  data[OVERALL_LABEL] = (
-    sum(item[0] for item in data.values()),
-    sum(item[1] for item in data.values()),
-    reduce(
-      (lambda x, y: x/y),
-       map(
-       (lambda x: x if x else 1),
-        map((lambda x: x[2]/100), data.values()))),
-  )
-    
-  # add something like activity ratio, (lt-pt rather than lt/pt) to
-  # determine where our biggest delays are.  This is more interesting
-  # than actual ar, since we don't really care about a low activity
-  # ratio if lt is negligible...
-  data =  OrderedDict((key, (*value, value[0]-value[1])) for key, value in data.items())
+    """
+    Add summary/roll-up data to the chart, and calculate activity ratio
+    """
+    data[OVERALL_LABEL] = (
+        sum(item[0] for item in data.values()),
+        sum(item[1] for item in data.values()),
+        reduce(
+            (lambda x, y: x / y),
+            map(
+                (lambda x: x if x else 1),
+                map((lambda x: x[2] / 100), data.values()))),
+    )
 
-  return data
+    # add something like activity ratio, (lt-pt rather than lt/pt) to
+    # determine where our biggest delays are.  This is more interesting
+    # than actual ar, since we don't really care about a low activity
+    # ratio if lt is negligible...
+    data = OrderedDict(
+        (key, (*value, value[0] - value[1])) for key, value in data.items())
+
+    return data
+
 
 def load(file):
-  """
-  Read a CSV file with columns (task), (lead time), (process time)
-  """
+    """
+    Read a CSV file with columns (task), (lead time), (process time)
+    """
 
-  data = OrderedDict()
-  with open(file, newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in reader:
-      if row[0][0] == "#" or len(row) != 4:
-        pass
+    data = OrderedDict()
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            if row[0][0] == "#" or len(row) != 4:
+                pass
 
-      try:
-        data[row[0]] = (int(row[1]), int(row[2]), int(row[3]))
-      except IndexError:
-        print("Not enough columns: "+",".join(row))
+            try:
+                data[row[0]] = (int(row[1]), int(row[2]), int(row[3]))
+            except IndexError:
+                print("Not enough columns: " + ",".join(row))
 
-  return data
+    return data
 
 
 @click.command()
 @click.option("-h", "--height", default=350, type=int,
-            help="Figure height")
+              help="Figure height")
 @click.option("-i", "--items", default=3, type=int,
-            help="Number of worst items to highlight")
+              help="Number of worst items to highlight")
 @click.option("-t", "--title", default="",
-            help="Figure title")
+              help="Figure title")
 @click.option("-u", "--units", default=list(UNITS.keys())[0],
-            type=click.Choice(UNITS.keys(), case_sensitive=False),
-            help="Display units (%s)"%", ".join((f"{k} ({v['text']})" for k, v in UNITS.items())))
+              type=click.Choice(UNITS.keys(), case_sensitive=False),
+              help=("Display units (%s)" % ", ".join(
+                  ((f"{k} ({v['text']})" for k, v in UNITS.items()))
+                  )))
 @click.option("-w", "--width", default=800, type=int,
-            help="Figure width")
+              help="Figure width")
 @click.option("-x", "--xrange", default=None, type=int,
-            help="Figure Max X value")
+              help="Figure Max X value")
 @click.argument("file")
 def main(file, title, width, height, xrange, units, items):
-  """Generate a HTML chart presenting timeline from a
-     Value Stream Mapping exercise
-  """
+    """Generate a HTML chart presenting timeline from a
+       Value Stream Mapping exercise
+    """
 
-  data = load(file)
-  data = convert(data, units)
-  data = augment(data)
+    data = load(file)
+    data = convert(data, units)
+    data = augment(data)
 
-  generate(data, title, width, height, xrange, units, items)
+    generate(data, title, width, height, xrange, units, items)
 
 
 if __name__ == "__main__":
-  main()
+    main()
